76a15b257a9020578f4383be5b9ffa42bcdbb041
Javier Talledo <javier.talledob@gmail.com>
2022-11-21 22:54:14 -0500

    init

---
+++ src/main/java/com/javier/azure/apppruebaazure3/AppPruebaAzure3Application.java
@@ -0,0 +1,13 @@
+package com.javier.azure.apppruebaazure3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPruebaAzure3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppPruebaAzure3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/javier/azure/apppruebaazure3/AppPruebaAzure3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javier.azure.apppruebaazure3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPruebaAzure3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
